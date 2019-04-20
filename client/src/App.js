5e408e2332a1371d1271c0ce1ea3d4c0ee76b28a
Remy <remington49@rocketmail.com>
2019-04-20 10:37:02 -0400

    ...:

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import NavBar2 from '../src/components/NavBar/NavBar2';
 // import logo from './logo.svg';
 import './App.css';
+import History from "./components/Cards/history"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <NavBar2/>
-
+        <History />
 
 
       </div>
---
+++ client/src/components/Cards/history.js
@@ -0,0 +1,38 @@
+import React from "react"
+
+
+
+
+function History(){
+
+return (
+
+    <div style={{float: "right", marginRight: 150, marginTop: 45}}>
+
+    <h2 className="card">History</h2>
+
+   <div className="card" style={{padding: 20}}> 
+
+    <div className="card" style={{width: 300, marginTop: 35}}>
+        <div className="card-body">
+             <h4>Client</h4>
+             <p>bqdubqwudbquwbduqwbdu</p>
+        </div>
+    </div><div className="card" style={{width: 300, marginTop: 35}}>
+        <div className="card-body">
+             <h4>Support Manager</h4>
+             <p>bqdubqwudbquwbduqwbdu</p>
+        </div>
+    </div><div className="card" style={{width: 300, marginTop: 35}}>
+        <div className="card-body">
+             <h4>Notes</h4>
+             <p>bqdubqwudbquwbduqwbdu</p>
+        </div>
+    </div>
+
+</div>
+    </div>
+)
+}
+
+export default History;
