7f31b63a495ce6f1849716360597e6ad2f540bf1
john.mappin89@gmail.com <john.mappin89@gmail.com>
2019-04-20 16:31:12 -0400

    added replyForm and modal

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,10 @@ import NavBar2 from '../src/components/NavBar/NavBar2';
 import NavBar1 from '../src/components/NavBar/NavBar1';
 // import logo from './logo.svg';
 import './App.css';
-
+// import ReplyForm from './components/Modals/Reply/ReplyForm'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import CreateTicketModal from '../src/components/Modals/CreateTicket/CreateTicketModal';
+import ReplyModal from './components/Modals/Reply/ReplyModal';
+// import CreateTicketModal from '../src/components/Modals/CreateTicket/CreateTicketModal';
 
 class App extends Component {
   render() {
@@ -13,7 +14,7 @@ class App extends Component {
       <div className="App">
         <NavBar1/>
         <NavBar2/>
-        <CreateTicketModal />
+        <ReplyModal/>
 
 
 
--- client/src/components/Rows/test.js
+++
@@ -1,1 +0,0 @@
-// remove or rename this
